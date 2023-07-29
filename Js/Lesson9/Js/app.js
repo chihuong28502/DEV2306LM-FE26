57e6bd93050350b14f6225f1efa564781bc88b3f
Chí Hưởng <>
2023-07-29 11:29:05 +0700

    update

--- Js/Lesson9/Js/app.js
+++ Js/Lesson9/Js/app.js
@@ -2,121 +2,160 @@
 var listNews = [
     {
         id : 1,
-        title: "Văn hóa uống trà của người Việt 1",
+        title: "Văn hóa uống trà của người Việt 1,",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 2,
-        title: "Văn hóa uống trà của người Việt 2",
+        title: "Văn hóa uống trà của người Việt 2,",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 3,
-        title: "Văn hóa uống trà của người Việt 3",
+        title: "Văn hóa uống trà của người Việt 3,",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 4,
-        title: "Văn hóa uống trà của người Việt 4",
+        title: "Văn hóa uống trà của người Việt 4,",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 5,
-        title: "Văn hóa uống trà của người Việt 5",
+        title: "Văn hóa uống trà của người Việt 5,",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 6,
-        title: "Văn hóa uống trà của người Việt 6 ok",
+        title: "Văn hóa uống trà của người Việt 6, ok",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 7,
-        title: "Văn hóa uống trà của người Việt 16",
+        title: "Văn hóa uống trà của người Việt 7,",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 8,
-        title: "Văn hóa uống trà của người Việt 26",
+        title: "Văn hóa uống trà của người Việt 8,",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 9,
-        title: "Văn hóa uống trà của người Việt 36",
+        title: "Văn hóa uống trà của người Việt 9,",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 10,
-        title: "Văn hóa uống trà của người Việt 61",
+        title: "Văn hóa uống trà của người Việt 10",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 11,
-        title: "Văn hóa uống trà của người Việt trang 2",
+        title: "Văn hóa uống trà của người Việt 11 11",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 12,
-        title: "Văn hóa uống trà của người Việt 6",
+        title: "Văn hóa uống trà của người Việt 12",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 13,
-        title: "Văn hóa uống trà của người Việt 6",
+        title: "Văn hóa uống trà của người Việt 13",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 14,
-        title: "Văn hóa uống trà của người Việt 6",
+        title: "Văn hóa uống trà của người Việt 14",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 15,
-        title: "Văn hóa uống trà của người Việt 6",
+        title: "Văn hóa uống trà của người Việt 15",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 16,
-        title: "Văn hóa uống trà của người Việt 6",
+        title: "Văn hóa uống trà của người Việt 16",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 17,
-        title: "Văn hóa uống trà của người Việt 6",
+        title: "Văn hóa uống trà của người Việt 17",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 18,
-        title: "Văn hóa uống trà của người Việt 6",
+        title: "Văn hóa uống trà của người Việt 18",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 19,
-        title: "Văn hóa uống trà của người Việt 6",
+        title: "Văn hóa uống trà của người Việt 19",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     },
     {
         id : 20,
-        title: "Văn hóa uống trà của người Việt 6",
+        title: "Văn hóa uống trà của người Việt 20",
+        content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
+        image : "../Images/x.png"
+    },
+    {
+        id : 21,
+        title: "Văn hóa uống trà của người Việt 21",
+        content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
+        image : "../Images/x.png"
+    },
+    {
+        id : 22,
+        title: "Văn hóa uống trà của người Việt 22",
+        content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
+        image : "../Images/x.png"
+    }
+    ,
+    {
+        id : 23,
+        title: "Văn hóa uống trà của người Việt 23",
+        content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
+        image : "../Images/x.png"
+    }
+    ,
+    {
+        id : 24,
+        title: "Văn hóa uống trà của người Việt 24",
+        content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
+        image : "../Images/x.png"
+    }
+    ,
+    {
+        id : 25,
+        title: "Văn hóa uống trà của người Việt 25",
+        content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
+        image : "../Images/x.png"
+    },
+    {
+        id : 26,
+        title: "Văn hóa uống trà của người Việt 26",
         content : "Văn hóa uống trà của người Việt là một phần không thể thiếu trong đời sống và văn hoá Việt Nam. Nó không chỉ....",
         image : "../Images/x.png"
     }
@@ -152,7 +191,7 @@ function searchItem(){
 }
 setListNew(listNews, "")
 var pageIndex = 1;
-var pageSize = 4;
+var pageSize = 6;
 var lengthListNews = listNews.length;
 //  0 1 2 / 3 4 5 / 6  
 function setPageNews(arr,pageNumber){
@@ -160,21 +199,21 @@ function setPageNews(arr,pageNumber){
     var start = (pageNumber - 1)* pageSize;
     var end =  pageNumber * pageSize;
     $('.list-news').html('');
-    for(let i=start  ; (i<end) && (i<count-1) ; i++){
+    for(let i=start  ; (i<end) && (i<=count-1) ; i++){
         setView(arr[i]);
     }
 }
-changePageNumber(5);
-
-// 
-
+changePageNumber(4);
 function changePageNumber(pageNumber){
     setPageNews(listNews, pageNumber)
 }
+
+// 
 function setPage(i){
+    // render HTML
     var Page = `
         <li>
-            <a href="" class="news-page-number">${i}</a>
+            <a  class="news-page-number" id ="page-number-${i}" onclick="clickPageNumbers()">${i}</a>
         </li>
     `
     $('.news-page-list-number').append(Page);
@@ -182,19 +221,38 @@ function setPage(i){
 var listPages = [];
 function getPageNumbers(){
     var cal = lengthListNews / pageSize ;
-    for(var i=1 ; i<=Math.floor(cal +1);i++){
-        listPages.push(i)
+    if(lengthListNews % pageSize ==0){
+        for(var i=1 ; i<=cal;i++){
+            listPages.push(i);
+        }
+    }else{
+        for(var i=1 ; i<=Math.floor(cal +1);i++){
+            listPages.push(i);
+        }
     }
     return listPages;
 }
 getPageNumbers()
 function setPageNew(){
+    // loop show html
     for(let i= 1 ; i<=listPages.length; i++){
-        setPage(i);
+        setPage([i]);
 
     }
 }
 setPageNew()
+
+function clickPageNumbers(){
+    
+    for(var i= 0 ; i<=listPages.length-1; i++){
+        var indexPage = document.getElementById(`page-number-${listPages[i]}`);
+        if(indexPage.content == listPages[i]){
+            changePageNumber(listPages[i]);
+        }
+        console.log(indexPage)
+    }
+}
+clickPageNumbers()
 
 
 
