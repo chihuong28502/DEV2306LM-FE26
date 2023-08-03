31512a78b4f7e2b38b4fb084182932e9d5690627
Chí Hưởng <>
2023-08-03 23:05:30 +0700

    done ls10

--- Js/LESSON10/LESSON10/JS/card.js
+++ Js/LESSON10/LESSON10/JS/card.js
@@ -21,7 +21,7 @@ var listProduct = [
         id : 3,
         title : "Sản phẩm 3",
         price : 300000,
-        priceView: 300.000,
+        priceView: "300.000",
         quantity :1,
         Image: "../Image/x.png",
         quality: 4
@@ -29,16 +29,16 @@ var listProduct = [
     {
         id : 4,
         title : "Sản phẩm 4",
-        price : 200000,
-        priceView: 200.000,
+        price : 400000,
+        priceView: "400.000",
         quantity :1,
         Image: "../Image/x.png",
         quality: 4
-    },
+    }
 ]
 function renderHtml(obj){
     var item= `
-        <div class="item boder id-${obj.id}">
+        <div class="item border-1 id-${obj.id}">
             <div class="d-flex justify-between" id="product-${obj.id}">
                 <div class="image-item w-23">
                     <img class="w-100" src="${obj.Image}" alt="">
@@ -53,21 +53,21 @@ function renderHtml(obj){
                     <button class="sub-1" style="padding: 0 1.5rem;">-1</button> 
                     <span class="item-quantity" id="product-quantity-${obj.id}">${obj.quantity}</span> 
                     <button id="xoa">xóa</button>
-                    <button class="add-1" style="padding: 0 1.5rem;">+1</button>
+                    <button class="add-1" data-quality="${obj.quality}" style="padding: 0 1.5rem;">+1</button>
                 </div>
             </div>
         </div>
     `
     $('.list-item').append(item);
 }
-function showHtml(){
+function handleShowHtml(){
     for(var i=0; i<listProduct.length; i++){
         renderHtml(listProduct[i]);
     }
-    sumPrice(listProduct)
+    handleSumPrice()
 }
-showHtml()
-function sumPrice(){
+handleShowHtml()
+function handleSumPrice(){
     var sum = 0 ;
     var price = 0;
     var quantity =0;
@@ -76,48 +76,43 @@ function sumPrice(){
     for(let i =1 ; i <= listProduct.length; i++){
         quantity = document.querySelector("#product-quantity-"+ i).textContent;
         price = document.querySelector("#product-price-"+ i).attributes.getNamedItem("data-price").value;
-        priceView = document.querySelector("#product-price-"+ i).textContent;        
-            total = price * parseInt(quantity);
-            totalNew = total.toLocaleString("VND", {style:"currency", currency:"VND"});
-            $("#product-price-"+ i).text(totalNew)
-            sum = sum + total;
-        
+        priceView = document.querySelector("#product-price-"+ i).textContent;     
+            
+        total = price * parseInt(quantity);
+        totalNew = total.toLocaleString("VND", {style:"currency", currency:"VND"});
+        $("#product-price-"+ i).text(totalNew)
+        sum = sum + total;
     }
     var formatSumPrice = sum.toLocaleString("jp-JP", {style:"currency", currency:"VND"})
     $('.total-price').text(formatSumPrice);
+    
 }
-var arrQuality = [];
-function getQuatity(){
-    for(var i = 0; i <listProduct.length; i++) {
-        arrQuality.push(listProduct[i].quality)
-    }
-    return arrQuality;
-}
-getQuatity()
-console.log('arrQuality: ', arrQuality)
+
 $('.add-1').click(function(){
     var quantity = $(this).siblings('span').text();
-
-    if(parseInt(quantity) == 10){
-        quantity = 10
+    var limitUpper = this.attributes.getNamedItem("data-quality").value;
+    console.log('limitUpper: ', limitUpper)
+    if(parseInt(quantity) >= limitUpper){
+        quantity = limitUpper
     }else{
         quantity = parseInt(quantity) + 1;
     }
     $(this).siblings('span').text(quantity);
-    sumPrice();
+    handleSumPrice();
 })
 
 $('.sub-1').click(function(){
     var quantity = $(this).siblings('span').text(); // quantity = 0
+    
     if((parseInt(quantity) < 1)){
-        this.parentElement.parentElement.parentElement.removeChild();
-        
+        var selectParentItem = this.parentElement.parentElement.parentElement;
+        selectParentItem.style.display = 'none';
     }else{
         quantity =(parseInt(quantity) - 1)
     }
     // quantity = (parseInt(quantity) < 1) ? 0 : (parseInt(quantity) - 1) ;
     $(this).siblings('span').text(quantity);
-    sumPrice();
+    handleSumPrice();
 
 })
 
